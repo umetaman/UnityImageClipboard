8e8c7710150e1a9cc86d3dac99f5ce6f119ecdee
umetaman <info@umetaman.com>
2020-07-09 18:27:54 +0900

    1st commit

---
+++ Assets/Scripts/NativePlugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class NativePlugin
+{
+    [DllImport("Clipboard")]
+    private static extern bool hasClipboardImage();
+    [DllImport("Clipboard")]
+    private static extern bool getClipboardImage(IntPtr buffer, ref int width, ref int height, ref int bitsPerPixel);
+
+    public static bool HasClipboardImage()
+    {
+        return hasClipboardImage();
+    }
+
+    public static Texture2D GetClipboardImage()
+    {
+        IntPtr buffer = IntPtr.Zero;
+        int width = 0;
+        int height = 0;
+        int bitsPerPixel = 0;
+
+        bool success = getClipboardImage(buffer, ref width, ref height, ref bitsPerPixel);
+
+        if(success == false || buffer == IntPtr.Zero)
+        {
+            throw new Exception("Failed to get image.");
+        }
+
+        Debug.Log(width);
+        Debug.Log(height);
+        Debug.Log(bitsPerPixel);
+
+        return null;
+    }
+}
---
+++ NativePlugin/Windows/Clipboard/dllmain.cpp
@@ -0,0 +1,76 @@
+﻿#define PLUGIN_API extern "C" __declspec(dllexport)
+
+// https://docs.microsoft.com/en-us/windows/win32/dataxchg/clipboard
+#include <Windows.h>
+
+PLUGIN_API bool hasClipboardImage() {
+	// クリップボードから取り出すときは開く
+	bool isOpened = OpenClipboard(NULL);
+	bool hasImage = false;
+
+	if (isOpened) {
+		hasImage = IsClipboardFormatAvailable(CF_DIB);
+	}
+
+	// 必ず閉じる
+	CloseClipboard();
+
+	return hasImage;
+}
+
+// ロックしたBITMAPINFOをクリップボードからとってくる。
+// 解放は必須である
+static BITMAPINFO* getLockedBitmapInfoPtr() {
+	HANDLE hClipboardData = GetClipboardData(CF_DIB);
+
+	LPVOID clipboardDataPtr = GlobalLock(hClipboardData);
+	return static_cast<BITMAPINFO*>(clipboardDataPtr);
+}
+
+// 画像サイズを取得する
+// C#側でメモリを確保するために必要
+PLUGIN_API void getClipboardImageSize(int* width, int* height, int* bitsPerPixel) {
+	bool isOpened = OpenClipboard(NULL);
+	if (isOpened) {
+		if (IsClipboardFormatAvailable(CF_DIB)) {
+			BITMAPINFO* bitmapInfoPtr = getLockedBitmapInfoPtr();
+
+			if (bitmapInfoPtr != nullptr) {
+				*width = bitmapInfoPtr->bmiHeader.biWidth;
+				*height = bitmapInfoPtr->bmiHeader.biHeight;
+				*bitsPerPixel = bitmapInfoPtr->bmiHeader.biBitCount;
+			}
+
+			GlobalUnlock(bitmapInfoPtr);
+		}
+	}
+
+	CloseClipboard();
+}
+
+PLUGIN_API bool getClipboardImage(unsigned char* buffer) {
+	bool isOpened = OpenClipboard(NULL);
+	bool successCopy = false;
+
+	if (isOpened) {
+		if (IsClipboardFormatAvailable(CF_DIB)) {
+			BITMAPINFO* bitmapInfoPtr = getLockedBitmapInfoPtr();
+
+			if (bitmapInfoPtr != nullptr) {
+				int width, height, bitsPerPixel;
+				getClipboardImageSize(&width, &height, &bitsPerPixel);
+
+				const unsigned char* pixelData = (unsigned char*)bitmapInfoPtr + bitmapInfoPtr->bmiHeader.biSize;
+				memcpy(buffer, pixelData, width * height * (bitsPerPixel / 8));
+
+				successCopy = true;
+			}
+
+			GlobalUnlock(bitmapInfoPtr);
+		}
+	}
+
+	CloseClipboard();
+
+	return successCopy;
+}

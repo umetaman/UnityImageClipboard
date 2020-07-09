be6334a1156979e5eb977cb970a8b3c7aaf02e0c
umetaman <info@umetaman.com>
2020-07-09 19:36:59 +0900

    Unityのバージョンを更新。C#側の実装。IntPtrをこの後どうすればいいかド忘れしたのでひとまずコミット

--- Assets/Scripts/NativePlugin.cs
+++ Assets/Scripts/NativePlugin.cs
@@ -11,7 +11,9 @@ public static class NativePlugin
     [DllImport("Clipboard")]
     private static extern bool hasClipboardImage();
     [DllImport("Clipboard")]
-    private static extern bool getClipboardImage(IntPtr buffer, ref int width, ref int height, ref int bitsPerPixel);
+    private static extern void getClipboardSize(ref int width, ref int height, ref int bitsPerPixel);
+    [DllImport("Clipboard")]
+    private static extern bool getClipboardImage(IntPtr buffer);
 
     public static bool HasClipboardImage()
     {
@@ -20,22 +22,33 @@ public static class NativePlugin
 
     public static Texture2D GetClipboardImage()
     {
-        IntPtr buffer = IntPtr.Zero;
         int width = 0;
         int height = 0;
         int bitsPerPixel = 0;
-
-        bool success = getClipboardImage(buffer, ref width, ref height, ref bitsPerPixel);
-
-        if(success == false || buffer == IntPtr.Zero)
+        getClipboardSize(ref width, ref height, ref bitsPerPixel);
+    
+        if(width * height < 1)
         {
-            throw new Exception("Failed to get image.");
+            throw new Exception("Not Support Image Size.");
         }
 
-        Debug.Log(width);
-        Debug.Log(height);
-        Debug.Log(bitsPerPixel);
+        int channel = bitsPerPixel / 8;
 
-        return null;
+        if(channel != 3 && channel != 4)
+        {
+            throw new Exception("Not Support Channel Count.");
+        }
+
+        // C#側のバッファを用意する
+        byte[] buffer = new byte[width * height * channel];
+        // GCによって移動しないように固定する。必ず開放する。
+        GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        // 確保したバッファのポインタ
+        IntPtr bufferPtr = handle.AddrOfPinnedObject();
+
+        bool successCopy = false;
+        successCopy = getClipboardImage(bufferPtr);
+        
+
     }
 }

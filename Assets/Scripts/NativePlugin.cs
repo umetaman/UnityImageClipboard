b75c35025d71c6a5f5591e4b9a3eb22d20861bab
umetaman <yuyaumeta@gmail.com>
2021-02-09 17:59:27 +0900

    Windows, macOSを#ifdefで分岐する

--- Assets/Scripts/NativePlugin.cs
+++ Assets/Scripts/NativePlugin.cs
@@ -59,32 +59,67 @@ public static class NativePlugin
 
         if (successCopy)
         {
-            Texture2D texture = new Texture2D(width, height, TextureFormat.BGRA32, false);
+#if UNITY_STANDALONE_WIN
+            Texture2D texture = createClipboardTextureWindows(width, height, channel, ref buffer);
+#elif UNITY_STANDALONE_OSX
+            Texture2D texture = createClipboardTextureOSX(width, height, channel, ref buffer);
+#endif
 
-            // BGRA
-            if (channel == 4)
-            {
-                texture.LoadRawTextureData(buffer);
-            }
-            // BGR
-            else if (channel == 3)
-            {
-                Color32[] pixels = new Color32[width * height];
-                for (int i = 0; i < pixels.Length; i++)
-                {
-                    pixels[i].b = buffer[channel * i];
-                    pixels[i].g = buffer[channel * i + 1];
-                    pixels[i].r = buffer[channel * i + 2];
-                    pixels[i].a = (byte)255;
-                }
-
-                texture.SetPixels32(pixels);
-            }
-
-            texture.Apply();
+            buffer = null;
             return texture;
         }
 
         return null;
+    }
+
+    private static Texture2D createClipboardTextureWindows(int width, int height, int channel, ref byte[] buffer)
+    {
+        Texture2D texture = new Texture2D(width, height, TextureFormat.BGRA32, false);
+
+        // BGRA
+        if (channel == 4)
+        {
+            texture.LoadRawTextureData(buffer);
+        }
+        // BGR
+        else if (channel == 3)
+        {
+            Color32[] pixels = new Color32[width * height];
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                pixels[i].b = buffer[channel * i];
+                pixels[i].g = buffer[channel * i + 1];
+                pixels[i].r = buffer[channel * i + 2];
+                pixels[i].a = (byte)255;
+            }
+
+            texture.SetPixels32(pixels);
+            pixels = null;
+        }
+
+        texture.Apply();
+        return texture;
+    }
+
+    private static Texture2D createClipboardTextureOSX(int width, int height, int channel, ref byte[] buffer)
+    {
+        Texture2D texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        Color32[] pixels = new Color32[width * height];
+
+        for(int i = 0; i < pixels.Length; i++)
+        {
+            byte alpha = channel == 4 ? buffer[i * channel + 3] : (byte)255;
+            pixels[i].a = alpha;
+
+            pixels[i].r = alpha != 0 ? (byte)(buffer[i * channel] * 255 / alpha) : buffer[i * channel];
+            pixels[i].g = alpha != 0 ? (byte)(buffer[i * channel + 1] * 255 / alpha) : buffer[i * channel + 1];
+            pixels[i].b = alpha != 0 ? (byte)(buffer[i * channel + 2] * 255 / alpha) : buffer[i * channel + 2];
+        }
+
+        texture.SetPixels32(pixels);
+        texture.Apply();
+        pixels = null;
+
+        return texture;
     }
 }
